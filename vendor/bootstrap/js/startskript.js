76bff6750c2d88e8cfceefbf3c357c0ce860108b
RubeldiRubelda <benjamin.kunz.ch@gmail.com>
2025-11-12 11:03:28 +0100

    Super

--- vendor/bootstrap/js/startskript.js
+++ vendor/bootstrap/js/startskript.js
@@ -114,13 +114,13 @@
         const bahnnr = data?.stationboard[0].category ?? "NaN";
         const bahnzahl = data?.stationboard[0].number ?? "NaN";
         const destination = data?.stationboard[0].to ?? "Unbekanntes Ziel";
-        const delay = data?.stationboard[0].stop.delay ?? "Ufpasse";
+        const delay1 = data?.stationboard[0].stop.delay ?? "Ufpasse";
         const departureTime = data?.stationboard[0].stop.departure ?? "Unbekannte Zeit"; // Ausgabe Format: 2025-11-07T14:10:00+0100
         document.getElementById("nr1").textContent = bahnnr + bahnzahl;
         document.getElementById("stationsname").textContent = "Fährt ab " + stationName + " nach " + destination;
-        console.log(delay);
-        if (delay > 0) {
-            document.getElementById("verspätung1").textContent = "Verspätung: +" + delay + " Min.";
+        console.log(delay1);
+        if (delay1 >= 2) {
+            document.getElementById("verspätung1").textContent = "Verspätung: +" + delay1 + " Min.";
         }
         const depDate = new Date(departureTime);
         document.getElementById("abfahrt1").textContent = "Abfahrt: " + depDate.toLocaleTimeString('de-CH', options);
@@ -136,8 +136,8 @@
         document.getElementById("nr2").textContent = bahnnr2 + bahnzahl2;
         document.getElementById("stationsname2").textContent = "Fährt ab " + stationName2 + " nach " + destination2;
         console.log(delay2);
-        if (delay > 0) {
-            document.getElementById("verspätung1").textContent = "Verspätung: +" + delay + " Min.";
+        if (delay2 > 0) {
+            document.getElementById("verspätung1").textContent = "Verspätung: +" + delay2 + " Min.";
         }
         const depDate2 = new Date(departureTime2);
         document.getElementById("abfahrt2").textContent = "Abfahrt: " + depDate2.toLocaleTimeString('de-CH', options);
@@ -152,6 +152,9 @@
         document.getElementById("nr3").textContent = bahnnr3 + bahnzahl3;
         document.getElementById("stationsname3").textContent = "Fährt ab " + stationName3 + " nach " + destination3;
         console.log(delay3);
+                if (delay3 > 0) {
+            document.getElementById("verspätung1").textContent = "Verspätung: +" + delay3 + " Min.";
+        }
         const depDate3 = new Date(departureTime3);
         document.getElementById("abfahrt3").textContent = "Abfahrt: " + depDate3.toLocaleTimeString('de-CH', options);
         /// ABFAHRT 3 ENDE ///
@@ -165,6 +168,9 @@
         document.getElementById("nr4").textContent = bahnnr4 + bahnzahl4;
         document.getElementById("stationsname4").textContent = "Fährt ab " + stationName4 + " nach " + destination4;
         console.log(delay4);
+                if (delay4 > 0) {
+            document.getElementById("verspätung1").textContent = "Verspätung: +" + delay4 + " Min.";
+        }
         const depDate4 = new Date(departureTime4);
         const options4 = { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'Europe/Berlin' };
         document.getElementById("abfahrt4").textContent = "Abfahrt: " + depDate4.toLocaleTimeString('de-CH', options);
@@ -180,6 +186,9 @@
         document.getElementById("stationsname5").textContent = "Fährt ab " + stationName5 + " nach " + destination5;
         const depDate5 = new Date(departureTime5);
         console.log(delay5);
+                if (delay5 >= 1) {
+            document.getElementById("verspätung1").textContent = "Verspätung: +" + delay5 + " Min.";
+        }
         const options5 = { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'Europe/Berlin' };
         document.getElementById("abfahrt5").textContent = "Abfahrt: " + depDate5.toLocaleTimeString('de-CH', options);
         /// ABFAHRT 5 ENDE ///
